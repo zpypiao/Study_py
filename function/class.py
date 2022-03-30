a7c3a76ecb9b7a3bba5c7ce9db263d7518286bb0
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-03-30 10:40:15 +0800

    Create class.py

---
+++ function/class.py
@@ -0,0 +1,10 @@
+class Person:
+	
+	#restart the object
+	def __init__(self),name,age):
+		self.name = name
+		self.age = age
+	
+	#translate the object to this str when print object
+	def __str__(self):
+		return 'Hello World'
