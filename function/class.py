ebf39340df3cda9c2e40725d49e6e5123531ed33
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-03-30 15:10:41 +0800

    Update class.py

--- function/class.py
+++ function/class.py
@@ -8,3 +8,18 @@ class Person:
 	#translate the object to this str when print object
 	def __str__(self):
 		return 'Hello World'
+
+class User:
+	
+	def __init__(self,name,pw):
+		self.name = name
+		if len(pw) >= 6:
+			self.__password = pw  #hiding info
+		else:
+			print('the pw is invalid')
+	
+	def get_pd(self):
+		return self.__password
+	
+	def __hello(self): #this function can not be use out of the group
+		print('Hello World')
