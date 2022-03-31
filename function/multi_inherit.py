8a6d34ef3febf9ae301e660d1ff3921af35a10aa
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-03-31 15:33:53 +0800

    Create multi_inherit.py

---
+++ function/multi_inherit.py
@@ -0,0 +1,20 @@
+#the any object's father is <object>
+class A(object):
+	
+	def test(self):
+		print('This is type A')
+		
+class B:
+	
+	def Test(self):
+		print('This is type B')
+		
+class C(A,B):
+#the grade of A is higher than B
+	
+	def test_c(self):
+		print('This is the type C')
+		
+c = C()
+C.__mro__ #print the father grade
+c.test() #follow the order number of grade
