19d17750859d00b0afa4c753d101ac2153d124d4
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-04-02 16:02:46 +0800

    Create factory_method_complete.py

---
+++ function/factory_method_complete.py
@@ -0,0 +1,40 @@
+class Person(object):
+	
+	def __init__(self,name):
+		self.name = name
+    
+	def work(self,axe_type):
+		axe = Factory.creat_axe(axe_type)
+		axe.cut_tree
+		
+class Axe(object):
+	
+	def __init(self,name):
+		self.name = name
+		
+	def cut_tree(self):
+		print('%s is cutting the tree.'%self.name)
+		
+class StoneAxe(Axe):
+	
+	def cut_tree(self):
+		print('%s is cutting the tree by stone.'%self.name)
+		
+class SteelAxe(Axe):
+	
+	def cut_tree(self):
+		print('%s is cutting the tree by steel.'%self.name)
+		
+class Factory(object):
+	
+	@staticmethod
+	def creat_axe(type):
+		if type == 'stone':
+			return StoneAxe('huagangyang')
+		elif type == 'steel':
+			return SteelAxe('taihejin')
+		else:
+			return False
+
+p = Person('prime')
+p.work('stone')
