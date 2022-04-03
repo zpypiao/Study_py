eac44f2a1662ab8bd9dc40f0a7bf5e2e05494a2c
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-04-03 10:27:12 +0800

    Create module_test.py

---
+++ function/module_test.py
@@ -0,0 +1,16 @@
+#this defeat from module import *
+__all__ = ['istr'] #this decide the function can be use in other file when import
+
+def istr(str):
+	if type(str) == <class 'str'>:
+		return True
+	else:
+		return False
+
+def this(a*):
+	return True
+	
+print('This is a test code',istr('acv')) #this will be excuted when import
+
+if __name__ == '__mian__':
+	print('This is a test code',istr('acv')) #when this is a file instead of a module,it will be excuted
