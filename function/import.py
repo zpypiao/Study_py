6ae16150a4b170fcba229e6296d989eef409d642
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-04-03 09:52:47 +0800

    Create import.py

---
+++ function/import.py
@@ -0,0 +1,13 @@
+#import module1,module2
+
+import random
+print(random.randint(0,5))
+
+from random import ranint
+print(randint(0,5))
+
+from random import*
+print(randint(0,5))
+
+import random as rm
+print(rm.randint(0,5))
