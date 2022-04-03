9c83ddb4de82916d14776c3cb5f7ed127f32d0c4
zpypiao <98138915+zpypiao@users.noreply.github.com>
2022-04-03 11:09:56 +0800

    Create setup_module.py

---
+++ function/setup_module.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name = 'ase.zip',version = '1.0',description = 'simple function',author = 'zpy',
+py_modules = ['my_package.module1','my_package.module2']
+	  
+
+	  
+	  
+python3 setup.py build
+python3 setup.py sdisk #you will get a .tar.gz
